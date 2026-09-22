c77ed85492076a8f1174932366946164878b0ad4
eseungq <eseungqq@gmail.com>
2026-09-22 12:12:10 +0900

    feat: profile 라우터 생성 및 템플릿 파일 추가

--- HW1/app.py
+++ HW1/app.py
@@ -5,3 +5,8 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return render_template("home.html")
+
+@app.route("/profile")
+def profile():
+    hobbies = ["음악 감상", "사진 촬영", "여행"]
+    return render_template("profile.html", hobbies=hobbies)
