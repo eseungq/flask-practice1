95774936a22c8ed25248e18f0a012566cd14a529
eseungq <eseungqq@gmail.com>
2026-09-22 10:59:25 +0900

    feat: age 라우트 변수 타입 지정

--- app.py
+++ app.py
@@ -13,3 +13,12 @@ def about():
 @app.route("/test/<text>")
 def route_sample(text):
     return f"<h1>{text}</h1>"
+
+@app.route("/age/<num>")
+def age_any(num):
+    return f"<h1>{num} 살 - 타입은 {type(num).__name__}</h1>"
+
+@app.route("/age2/<int:num>")
+def age_int(num):
+    return f"<h1>{num} 살 - 타입은 {type(num).__name__}</h1>"
+
